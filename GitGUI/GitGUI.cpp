b1c56efb86eb3a94537d51eaa86b40e22b3360f3
hyanghe <hyanghe@126.com>
2019-02-19 11:33:41 -0500

    added more code for testing
    added more code for testing

--- GitGUI/GitGUI.cpp
+++ GitGUI/GitGUI.cpp
@@ -8,6 +8,8 @@ int main()
 {
     std::cout << "Hello World!\n"; 
 	std::cout << "This is GIT GUI!\n";
+
+	std::cout << "To check whether the GIT GUI works!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
